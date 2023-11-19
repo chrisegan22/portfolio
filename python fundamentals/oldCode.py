6125df90cb11e673d33f4c2dff31b4e28493daae
Christopher Egan <68412746+chrisegan22@users.noreply.github.com>
2023-11-19 19:58:50 +0100

    python fund
    python fund

---
+++ python fundamentals/oldCode.py
@@ -0,0 +1,38 @@
+# Running Python code
+# hello = "Hello!"
+# print("Hello")
+# print("adsfsdafdsa")
+# text = input("Type some code: ")
+# print(text)
+# age = 27
+# age = 28
+# money = 10.10
+# print(type(money))
+# isGameOver = False
+# name = "Chris"
+# name = 'Chris'
+# Assignment: =
+# age = 27
+# first_name = "Chris"
+# health = 100
+# num_lives = 3
+# Arithmetic: + - * / % // **
+# new_age = age + 1
+# age = age + 1
+# age += 1
+# full_name = first_name + "Egan"
+# Comparison: == != >= > <= <
+# is_game_over = health <= 0
+# print(is_game_over)
+# Logical: and or not
+# is_game_over = health <= 0 and num_lives <= 0
+# is_not_game_over = not is_game_over
+# Lists
+# inventory = ["Axe", "Food", "Helmet",]
+# food = inventory [1]
+# inventory[1] = "Fruit"
+# first_two = inventory [0:2]
+# inventory.append("Water")
+# inventory.remove("Axe")
+# inventory.insert(1,"Knife")
+# print(inventory)
